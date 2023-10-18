178b93ec58e6a8f004de24213190eea15adac48d
mohammed-coding404 <mohammed.coding404@gmail.com>
2023-10-18 21:25:06 +0300

    upload

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/*global $, alert, console*/
+
+$(function () {
+
+    'use strict';
+    // triger nice scroll plugin
+    $('html').niceScroLL();
+
+    //change header hieght 
+
+    $('.header').height($(window).height());
+});
